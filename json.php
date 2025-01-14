7a8e30536a346016664063176f8ef51ef20aa738
M_CODER <84375305+mcodersir@users.noreply.github.com>
2025-01-15 03:07:15 +0330

    Delete json.php

--- json.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-require_once '../functions.php';
-$prices = getAllPrices();
-echo json_encode($prices, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
